6c0066a6f00ec003b2fc7e192678a1ead8005dc7
Rajdeep-deycreator <deyrajdeep569@gmail.com>
2025-04-16 16:38:23 +0530

    update

---
+++ an.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkpdhDngA9Rv2FpRnjt-yujRqm5gMgxzE",
+  authDomain: "adinet-1b5d2.firebaseapp.com",
+  projectId: "adinet-1b5d2",
+  storageBucket: "adinet-1b5d2.firebasestorage.app",
+  messagingSenderId: "295798421295",
+  appId: "1:295798421295:web:10e67b29ff8b7a477153da",
+  measurementId: "G-HQ2WFCBW0R"
+};
+ 
+firebase.initializeApp(firebaseConfig);
+const an=firebase.analytics();
+  
